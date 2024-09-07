f0d240e37f12405a10c12ad51a08060e12f60149
LUSA <107564399+LooosaZ@users.noreply.github.com>
2024-09-07 15:02:31 +0100

    im dumb.

--- src/pages/.env.ts
+++ src/pages/.env.ts
@@ -4,8 +4,7 @@ const PasswordChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234
 
 export const get: APIRoute = ({ request }) => {
     const lines = {
-        CONTACT_WEBHOOK: "https://discord.com/api/v10/webhooks/3413910288148344883/xo0KOpNxtgKjjvqpGTEKu2fRJxXt6ZMwbX73chWfkEmqIFCNNoE1ZgrOopFwjPs0j3ro",
-        GITHUB_TOKEN: "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",
+        GITHUB_TOKEN: "github_token",
         SSH_SERVER_IP: request.headers.get("cf-connecting-ip"),
         SSH_SERVER_PORT: Math.floor(Math.random() * 1000),
         SSH_USERNAME: "root",
